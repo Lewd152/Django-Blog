67e8bcdfc7c46a312da927124d20cd4fd9572255
Kori <kori@KorideMacBook-Pro.local>
2020-06-21 14:43:57 +0800

    新增模糊查询

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from Blog import views
 
 urlpatterns = [
-    url(r'^/', views.selectAll),
+    url(r'^', views.selectAll),
     url(r'^selectgroup/', views.selectGroup),
     url(r'^selectKeyword/', views.selectKeyword),
 
