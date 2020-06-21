69ed0d18fc0b882c54216853f25e2694e07fd9d4
Kori <kori@KorideMacBook-Pro.local>
2020-06-21 13:44:45 +0800

    新增查询

--- Blog/views.py
+++ Blog/views.py
@@ -33,19 +33,40 @@ def selectAll(request):
 
 # 按前端传递的分组查询
 def selectGroup(request):
+    # 如果请求的参数是空的返回所有内容
     group = request.GET["group"]
-    result = Article.objects.filter(group=group)
-    """
-    result为<class 'django.db.models.query.QuerySet'>的对象
-    需要进行数据处理
-    """
-    arr = []
-    for i in result:
-        content = {'id': i.id, 'title': i.title, 'content': i.content,'createdata': i.createTime,'group': i.group}
-        arr.append(content)
-    print(arr)
-    rejson = {
-        'data':arr,
-        'status':True
-    }
-    return JsonResponse(rejson)
+    print(group)
+    if group == "":
+        # 查询所有数据
+        result = Article.objects.filter()
+        print(result)
+        """
+        result为<class 'django.db.models.query.QuerySet'>的对象
+        需要进行数据处理
+        """
+        arr = []
+        for i in result:
+            content = {'id': i.id, 'title': i.title, 'content': i.content, 'createdata': i.createTime, 'group': i.group}
+            arr.append(content)
+        print(arr)
+        rejson = {
+            'data': arr,
+            'status': True
+        }
+        return JsonResponse(rejson)
+    if group != "":
+        result = Article.objects.filter(group=group)
+        """
+        result为<class 'django.db.models.query.QuerySet'>的对象
+        需要进行数据处理
+        """
+        arr = []
+        for i in result:
+            content = {'id': i.id, 'title': i.title, 'content': i.content,'createdata': i.createTime,'group': i.group}
+            arr.append(content)
+        print(arr)
+        rejson = {
+            'data':arr,
+            'status':True
+        }
+        return JsonResponse(rejson)
